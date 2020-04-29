03f9e0e33e2b93a3791922be902c3d2bf691c49e
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-04-29 11:09:57 +0900

    [FEATURE] createPost and updatePost

--- src/prisma.js
+++ src/prisma.js
@@ -1,14 +1,62 @@
-import { Prisma } from "prisma-binding";
+import { Prisma } from 'prisma-binding';
 
 const prisma = new Prisma({
-  typeDefs: "src/generated/prisma.graphql",
-  endpoint: "http://localhost:4466"
+  typeDefs: 'src/generated/prisma.graphql',
+  endpoint: 'http://localhost:4466',
 });
 
-prisma.query.users(null, "{ id name email posts { id title }}").then((data) => {
-  console.log(JSON.stringify(data, undefined, 2));
-});
+// --------------- query ---------------
+// prisma.query.users(null, '{ id name email posts { id title }}').then((data) => {
+//   console.log(JSON.stringify(data, undefined, 2));
+// });
 
-prisma.query.comments(null, "{ id text author { id name }}").then((data) => {
-  console.log(JSON.stringify(data, undefined, 2));
-});
+// prisma.query.comments(null, '{ id text author { id name }}').then((data) => {
+//   console.log(JSON.stringify(data, undefined, 2));
+// });
+
+// --------------- mutation ---------------
+// prisma.mutation
+//   .createPost(
+//     // first argument
+//     {
+//       data: {
+//         title: 'New post from prisma-binding',
+//         body: 'New post from prisma-binding',
+//         published: true,
+//         author: {
+//           connect: {
+//             id: 'ck9gjc06700dt0742plugk2qo',
+//           },
+//         },
+//       },
+//     },
+//     // second argument
+//     '{id title body published}'
+//   )
+//   .then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2));
+//   });
+
+prisma.mutation
+  .updatePost(
+    // first argument
+    {
+      data: {
+        body: 'Updated by prisma-binding v2',
+      },
+      where: {
+        id: 'ck9kossfp005m0742mj3xb5up',
+      },
+    },
+    // second argument
+    '{id}'
+  )
+  .then((data) => {
+    return prisma.query.posts(
+      null,
+      '{ id title body published author { id name }}'
+    );
+  })
+  .then((data) => {
+    console.log(JSON.stringify(data, undefined, 2));
+  });
