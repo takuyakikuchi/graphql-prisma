2cbf7d9d6e482e644bd8e94d452893714df91b05
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-05-24 16:08:15 +0900

    [Refactor](Comment): Subscription to prisma

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,21 +1,33 @@
 const Subscription = {
   comment: {
-    subscribe(parent, { postId }, { db, pubsub }, info) {
-      const postExist = db.posts.find(
-        (post) => post.id === postId && post.published
+    subscribe(parent, { postId }, { prisma }, info) {
+      return prisma.subscription.comment(
+        {
+          where: {
+            node: {
+              post: {
+                id: postId,
+              },
+            },
+          },
+        },
+        info
       );
+      // const postExist = db.posts.find(
+      //   (post) => post.id === postId && post.published
+      // );
 
-      if (!postExist) {
-        throw new Error("Post with given ID does not exist.");
-      }
+      // if (!postExist) {
+      //   throw new Error("Post with given ID does not exist.");
+      // }
 
-      return pubsub.asyncIterator(`comment ${postId}`);
+      // return pubsub.asyncIterator(`comment ${postId}`);
     },
   },
 
   post: {
     subscribe(parent, args, { pubsub }, info) {
-      return pubsub.asyncIterator("post");
+      return pubsub.asyncIterator('post');
     },
   },
 };
