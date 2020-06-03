b9e9ff5445840768ea0c4c0553a9a4f86e2a040a
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-06-03 09:54:03 +0900

    [FEATURE](UpdatePost): Auth token verification

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -56,9 +56,11 @@ const Mutation = {
     };
   },
 
-  async updateUser(parent, args, { prisma }, info) {
+  async updateUser(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+
     return prisma.mutation.updateUser({
-      where: { id: args.id },
+      where: { id: userId },
       data: args.data,
       info,
     });
