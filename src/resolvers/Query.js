de89f3efdc101c3a882d07061636e4aad7bbb3e2
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-06-06 15:40:41 +0900

    [FEATURE](Query): myPosts

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -5,6 +5,61 @@ const Query = {
     return prisma.query.comments(null, info);
   },
 
+  myPosts(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request);
+
+    const operationArgs = {
+      where: {
+        author: {
+          id: userId,
+        },
+      },
+    };
+
+    if (args.query) {
+      operationArgs.where.OR = [
+        {
+          title_contains: args.query,
+        },
+        {
+          body_contains: args.query,
+        },
+      ];
+    }
+
+    return prisma.query.posts(operationArgs, info);
+  },
+
+  async post(parent, args, { prisma, request }, info) {
+    // Second argument is for throwing error when there is no userId
+    const userId = getUserId(request, false);
+
+    const posts = await prisma.query.posts(
+      {
+        where: {
+          id: args.id,
+          OR: [
+            {
+              published: true,
+            },
+            {
+              author: {
+                id: userId,
+              },
+            },
+          ],
+        },
+      },
+      info
+    );
+
+    if (posts.length === 0) {
+      throw new Error('Post not found');
+    }
+
+    return posts[0];
+  },
+
   posts(parent, args, { prisma }, info) {
     const operationArgs = {
       where: {
@@ -43,36 +98,6 @@ const Query = {
     }
 
     return prisma.query.users(null, info);
-  },
-
-  async post(parent, args, { prisma, request }, info) {
-    // Second argument is for throwing error when there is no userId
-    const userId = getUserId(request, false);
-
-    const posts = await prisma.query.posts(
-      {
-        where: {
-          id: args.id,
-          OR: [
-            {
-              published: true,
-            },
-            {
-              author: {
-                id: userId,
-              },
-            },
-          ],
-        },
-      },
-      info
-    );
-
-    if (posts.length === 0) {
-      throw new Error('Post not found');
-    }
-
-    return posts[0];
   },
 };
 
