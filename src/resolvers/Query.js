00843cd95ee573e91f3fb03d16986246b808082d
Takuya Kikuchi <kikuchi.takuya.dbce8@gmail.com>
2020-06-06 15:21:15 +0900

    [FEATURE](Posts query): Fetch published posts

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -6,36 +6,24 @@ const Query = {
   },
 
   posts(parent, args, { prisma }, info) {
-    const operationArgs = {};
+    const operationArgs = {
+      where: {
+        published: true,
+      },
+    };
 
     if (args.query) {
-      operationArgs.where = {
-        OR: [
-          {
-            title_contains: args.query,
-          },
-          {
-            body_contains: args.query,
-          },
-        ],
-      };
+      operationArgs.where.OR = [
+        {
+          title_contains: args.query,
+        },
+        {
+          body_contains: args.query,
+        },
+      ];
     }
 
     return prisma.query.posts(operationArgs, info);
-
-    // if (!args.query) return db.posts;
-
-    // return db.posts.filter((post) => {
-    //   const titleMatched = post.title
-    //     .toLowerCase()
-    //     .includes(args.query.toLowerCase());
-
-    //   const bodyMatched = post.body
-    //     .toLowerCase()
-    //     .includes(args.query.toLowerCase());
-
-    //   return titleMatched || bodyMatched;
-    // });
   },
 
   users(parent, args, { prisma }, info) {
